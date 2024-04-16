65e0a9021a6f598c3f64954f3770079ef01c4917
unknown <abhi02167@gmail.com>
2024-04-16 03:22:09 -0700

    first commit

---
+++ Model/emp.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema({
+    name:String,
+    location:String,
+    position:String,
+    salary:Number
+})
+
+const empmodel = mongoose.model('emp',schema);
+module.exports = empmodel;
